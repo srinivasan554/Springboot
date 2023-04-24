d0497e803a0d4433e0eff86e6efcfbc5f87745a4
srinivasan554 <51996411+srinivasan554@users.noreply.github.com>
2023-04-24 18:26:07 +0530

    Add files via upload

---
+++ 01-spring-boot-demo/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    //expose / to return hello world;
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+    @GetMapping("/s")
+    public  String hidden(){
+        return "Hidden page:)";
+    }
+}
